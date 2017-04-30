75e112b024b9b719b32068bc7dff2acc920bc00e
Adam Pasternack <adam.pasternack@huntington.com>
2017-04-29 23:59:02 -0400

    yay

---
+++ app/src/main/java/com/adampasternack/textfielddemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.adampasternack.textfielddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void loginClicked(View view) {
+
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        Log.i("Username", usernameEditText.getText().toString());
+
+        Log.i("Username", passwordEditText.getText().toString());
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
